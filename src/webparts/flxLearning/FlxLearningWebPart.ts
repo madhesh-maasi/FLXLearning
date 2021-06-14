679ba7121d36cda354b73c3cffdf2bfa81dedeac
madhesh <madheshmaasi3@gmail.com>
2021-06-14 18:59:08 +0530

    nishanth design changes

--- src/webparts/flxLearning/FlxLearningWebPart.ts
+++ src/webparts/flxLearning/FlxLearningWebPart.ts
@@ -20,6 +20,8 @@ var alertify: any = require("../../ExternalRef/js/alertify.min.js");
 
 
  import * as $ from "jquery";
+ import * as moment from "moment";  
+ var Badgingdays="";
  var itemid = "";
  var listUrl = ""
  let LGUID = "";
@@ -89,7 +91,7 @@ export default class FlxLearningWebPart extends BaseClientSideWebPart<IFlxLearni
    <div class="q-link-title">Education</div>
    </div>-->
    
-  
+    
    </div>
    
    </div>  
@@ -383,7 +385,7 @@ $("#btnUpdateLearn").click(function(){
  
   $(document).on("change", "#File1FLXlearnEdit", function () {
     var _URL = window.URL;
-    var file, img;
+    var file, img;  
     if ((file = this.files[0])) {
         img = new Image();
         img.onload = function () {
@@ -421,15 +423,15 @@ $("#btnUpdateLearn").click(function(){
   
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
+    return Version.parse('1.0');  
+  }   
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription  
           },
           groups: [
             {
@@ -441,13 +443,24 @@ $("#btnUpdateLearn").click(function(){
               ]
             }
           ]
-        }
+        }  
       ]                                        
     };
   }
 }
 async function getadminfromsite() {
   $(".loader-section").show();
+
+
+  var bag=[];
+  let listLocation  = await sp.web.getList(listUrl + "Badging").items.get(); 
+  listLocation.forEach((li) => {
+   bag.push(li.Days); 
+   console.log(bag);
+  });
+  Badgingdays= bag[0];
+  console.log(Badgingdays);
+
   var AdminInfo = [];
   await sp.web.siteGroups
     .getByName("FLX Admins")
@@ -469,6 +482,8 @@ async function getadminfromsite() {
       $(".loader-section").hide();
     });
     $(".loader-section").hide();
+
+
 }
 function FetchFLXLearning() {
   $(".loader-section").show();
@@ -496,23 +511,23 @@ list.get().then(l => {
         const imageUrl = itemImage.serverRelativeUrl || "";
           
         if (item.OpeningNewTab === true) {
-          html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
+          html += `<div class="newlrn"><span class="newlearn newslearn${i}">New</span><div class = "q-link m-3 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
             <span class="editimageflxLearn" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id ="${item.ID}"></span></div>
             <a data-interception="off" href="${item.URL}" target="_blank"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a data-interception="off" class="" href="${item.URL}" target="_blank">
-            <div class="q-link-title">${item.Title}</div></a></div>`
+            <div class="q-link-title">${item.Title}</div></a></div></div>`
           // console.log(items)
         }
         else { 
-          html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
+          html += `<div class="newlrn"><span class="newlearn newslearn${i}">New</span><div class = "q-link m-3 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
           <span class="editimageflxLearn" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id="${item.ID}"></span>
           </div>
             <a href="${item.URL}"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a class="" href="${item.URL}">
-            <div class="q-link-title ">${item.Title}</div></a></div>`
+            <div class="q-link-title ">${item.Title}</div></a></div></div>`
         }             
       }   
 
       if(items.length>=0){
-        html+=`<div class="card text-center flxlearncursor m-2" id="add-link" style="width: 9rem; height:10.5rem ;border-radius:0">
+        html+=`<div class="card text-center flxlearncursor m-2 p-2 mt-3 " id="add-link" style="width: 9rem; height:10.5rem ;border-radius:0">
         <div class="card-body my-4">
         <span class="learn-add-icon " data-bs-toggle="modal" data-bs-target="#staticBackdroptwo"></span>
         <p class="learn-title my-2">Add Link</p>
@@ -530,25 +545,46 @@ list.get().then(l => {
             const imageUrl = itemImage.serverRelativeUrl || "";
               
             if (item.OpeningNewTab === true) {
-              html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
+              html += `<div class="newlrn"><span class="newlearn newslearn${i}">New</span><div class = "q-link m-3 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
                 <span class="editimageflx" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id ="${item.ID}"></span></div>
                 <a data-interception="off" href="${item.URL}" target="_blank"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a data-interception="off" class="" href="${item.URL}" target="_blank">
-                <div class="q-link-title">${item.Title}</div></a></div>`
+                <div class="q-link-title">${item.Title}</div></a></div></div>`
               // console.log(items)
-            }
-            else { 
-              html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
+            }         
+            else {    
+              html += `<div class="newlrn"><span class="newlearn newslearn${i}">New</span><div class = "q-link m-3 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
               <span class="editimageflx" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id="${item.ID}"></span>
               </div>
                 <a href="${item.URL}"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a class="" href="${item.URL}">
-                <div class="q-link-title ">${item.Title}</div></a></div>`
+                <div class="q-link-title ">${item.Title}</div></a></div> </div>`
             }             
           }   
           var element = document.getElementById("learnedit");
       element.innerHTML = html;
-          $("#ViewAll").hide();
-          $("#ShowVisible").hide();  
+          $("#ViewAll").hide();   
+          $("#ShowVisible").hide();     
         }
+        var count;
+ for(var i=0;i<items.length;i++){
+   count=i;
+   var today = new Date();
+   var startdate=new Date(items[i].Created);
+       var sdate=new Date(items[i].Created);
+       var Edate=sdate.setDate(sdate.getDate() + parseInt(Badgingdays));
+ var enddate=new Date(Edate);
+ var startdatemt=moment(startdate).format("YYYY-MM-DD");
+ var enddatemt=moment(enddate).format("YYYY-MM-DD");
+ var todaymt=moment(today).format("YYYY-MM-DD");
+ 
+       if(todaymt >= startdatemt && todaymt < enddatemt || todaymt > startdatemt && todaymt <= enddatemt){
+ 
+ $(".newslearn"+count).show();   
+ }  
+ else{    
+   $(".newslearn"+count).hide(); 
+
+ }
+ }
 
     })
     $(".loader-section").hide();
@@ -575,7 +611,9 @@ list.get().then(l => {
     
   console.log(items);
   
+  
  })
+ 
  $(".loader-section").hide();
 }
 
@@ -731,7 +769,7 @@ function FetchFLXLearningAll() {
   let list = sp.web.lists.getByTitle("FLXLearning");
 list.get().then(l => {
     console.log("List Id: " + l.Id);
-    LGUID=l.Id;
+    LGUID=l.Id;  
 }); 
   var html = "";
   
@@ -744,37 +782,58 @@ list.get().then(l => {
       for (var i = 0; i < items.length; i++) {
         const item = items[i];
         const itemImage = JSON.parse(item.Image) || {};
-        const serverUrl = itemImage.serverUrl || "";
+        const serverUrl = itemImage.serverUrl || "";  
         const imageUrl = itemImage.serverRelativeUrl || "";
-          
+              
         if (item.OpeningNewTab === true) {
-          html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
+          html += `<div class="newlrn"><span class="newlearn newslearn${i}">New</span><div class = "q-link m-3 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
             <span class="editimageflxLearn" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id ="${item.ID}"></span></div>
             <a data-interception="off" href="${item.URL}" target="_blank"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a data-interception="off" class="" href="${item.URL}" target="_blank">
-            <div class="q-link-title">${item.Title}</div></a></div>`
+            <div class="q-link-title">${item.Title}</div></a></div></div>`
           // console.log(items)
-        }
-        else { 
-          html += `<div class = "q-link m-2 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
+        }  
+        else {    
+          html += `<div class="newlrn"><span class="newlearn newslearn${i}">New</span><div class = "q-link m-3 text-center p-2"><div class="iconaddlearn text-end py-1 px-2">
           <span class="editimageflxLearn" data-bs-toggle="modal" data-bs-target="#staticBackdropone" data-id="${item.ID}"></span>
           </div>
             <a href="${item.URL}"><img class="q-link-img" src="${serverUrl}${imageUrl}" alt="img"/></a><a class="" href="${item.URL}">
-            <div class="q-link-title ">${item.Title}</div></a></div>`
+            <div class="q-link-title ">${item.Title}</div></a></div></div>`
         }             
       }   
-
+   
       if(items.length>=0){
-        html+=`<div class="card text-center flxlearncursor m-2" style="width: 9rem; height:10.5rem ;border-radius:0">
+        html+=`<div class="card text-center flxlearncursor m-2 p-2 mt-3 " style="width: 9rem; height:10.5rem ;border-radius:0">
         <div class="card-body my-4">
         <span class="learn-add-icon " data-bs-toggle="modal" data-bs-target="#staticBackdroptwo"></span>
         <p class="learn-title my-2">Add Link</p>
         </div>
       </div>`
       }   
-
+      
 
       var element = document.getElementById("learnedit");
       element.innerHTML = html;
+      var count;
+ for(var i=0;i<items.length;i++){
+   count=i;
+   var today = new Date();   
+   var startdate=new Date(items[i].Created);
+       var sdate=new Date(items[i].Created);
+       var Edate=sdate.setDate(sdate.getDate() + parseInt(Badgingdays));
+ var enddate=new Date(Edate);
+ var startdatemt=moment(startdate).format("YYYY-MM-DD");
+ var enddatemt=moment(enddate).format("YYYY-MM-DD");
+ var todaymt=moment(today).format("YYYY-MM-DD");
+ 
+       if(todaymt >= startdatemt && todaymt < enddatemt || todaymt > startdatemt && todaymt <= enddatemt){
+ 
+ $(".newslearn"+count).show();   
+ }  
+ else{    
+   $(".newslearn"+count).hide(); 
+
+ }
+ }
       
 
     })
